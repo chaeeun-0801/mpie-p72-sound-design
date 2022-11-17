b8181ad1b86527df0cbd88ac4f10bf81b35dee17
chaeeun-0801 <ck1071@york.ac.uk>
2022-11-17 13:24:19 +0000

    audio footstep

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown (KeyCode.S)||Input.GetKeyDown (KeyCode.A) || Input. GetKeyDown(KeyCode.D))
+        {
+            AudioSource footstep = GetComponent<AudioSource>();
+            footstep.Play();
+        }
+    }
+}
